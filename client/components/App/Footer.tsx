22b44cd7cae08f4ad0b7429a9af0fea81c4c1857
Andriy Drobenyuk <adb@mcs.be>
2019-02-20 10:29:01 +0200

    Fixed issues with footer

--- client/components/App/Footer.tsx
+++ client/components/App/Footer.tsx
@@ -4,16 +4,16 @@ import withStyles, { WithSheet } from 'react-jss';
 import Navigation from '../Navigation/View';
 import { darkGray } from '../../constants/styles';
 
-interface IProps extends WithSheet<typeof styles, {}, {}> {};
+interface Props extends WithSheet<typeof styles, {}, {}> {}
 
 const styles = {
     ['app-footer']: {
         backgroundColor: darkGray,
     },
-}
+};
 
-const AppFooter: React.SFC<IProps> = ({ classes }) => (
-    <footer className={classes["app-footer"]}>
+const AppFooter: React.SFC<Props> = ({ classes }) => (
+    <footer className={classes['app-footer']}>
         <Navigation />
     </footer>
 );
